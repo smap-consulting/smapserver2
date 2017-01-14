13389640c7ad76ffa941165de5a1bbee1c84e3bc
Neil Penman <neilpenman@gmail.com>
2017-01-14 23:48:08 +0300

    minor changes to options editor

--- sdDAL/src/org/smap/sdal/model/ExportForm.java
+++ sdDAL/src/org/smap/sdal/model/ExportForm.java
@@ -3,8 +3,8 @@ package org.smap.sdal.model;
 public class ExportForm {
 	public int sId;				// Passed by client
 	public int fId;				// Passed by client
-
 	public int fromQuestionId;	// Temporary variable
+	
 	public String table;		// Temporary variable
 	public int parent;			// Temporary variable
 	public int surveyLevel;		// Temporary variable - set to 0 for the first survey encountered
