3763307f6b0df1c1f36bb25f84e6ea9975efdb91
Neil Penman <neilpenman@gmail.com>
2017-05-28 17:02:28 +0100

    fix issue with delete option appearing to delete more than one option in editor

--- sdDAL/src/org/smap/sdal/Utilities/GeneralUtilityMethods.java
+++ sdDAL/src/org/smap/sdal/Utilities/GeneralUtilityMethods.java
@@ -3092,7 +3092,7 @@ public class GeneralUtilityMethods {
 	public static int getListId(Connection sd, int sId, String name) throws SQLException {
 		int listId = 0;
 		
-		String cleanName = GeneralUtilityMethods.cleanName(name, true, false, false);
+		//String cleanName = GeneralUtilityMethods.cleanName(name, true, false, false);
 		PreparedStatement pstmtGetListId = null;
 		String sqlGetListId = "select l_id from listname where s_id = ? and name = ?;";
 
@@ -3102,7 +3102,7 @@ public class GeneralUtilityMethods {
 		try {
 			pstmtGetListId = sd.prepareStatement(sqlGetListId);
 			pstmtGetListId.setInt(1, sId);
-			pstmtGetListId.setString(2, cleanName);
+			pstmtGetListId.setString(2, name);
 			
 			log.info("SQL: Get list id: " + pstmtGetListId.toString());
 			ResultSet rs = pstmtGetListId.executeQuery();
@@ -3112,7 +3112,7 @@ public class GeneralUtilityMethods {
 				
 				pstmtListName = sd.prepareStatement(sqlListName, Statement.RETURN_GENERATED_KEYS);
 				pstmtListName.setInt(1, sId);
-				pstmtListName.setString(2, cleanName);
+				pstmtListName.setString(2, name);
 				
 				log.info("SQL: Create list name: " + pstmtListName.toString());
 				
