2b22b8661678cb8df51ec2326b0afb58f4190a11
Neil Penman <neilpenman@gmail.com>
2017-06-19 18:21:27 +0200

    Show select multiple questions in browse results

--- sdDAL/src/org/smap/sdal/model/ChartItem.java
+++ sdDAL/src/org/smap/sdal/model/ChartItem.java
@@ -19,10 +19,14 @@ along with SMAP.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.smap.sdal.model;
 
+import java.util.ArrayList;
+
 public class ChartItem {
 	public int qIdx;
 	public String type;
 	public String name;
 	public String dataLabel;
 	public int l_id;
+	public ArrayList<String> choices;
+	public ArrayList<String> choiceNames;
 }
