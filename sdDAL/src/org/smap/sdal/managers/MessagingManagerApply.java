208d1ca5a1b0b5223abf3c1d154613631842ff59
Neil Penman <neilpenman@gmail.com>
2018-03-24 18:30:01 +0000

    Add survey level manifest changes to notifications

--- sdDAL/src/org/smap/sdal/managers/MessagingManagerApply.java
+++ sdDAL/src/org/smap/sdal/managers/MessagingManagerApply.java
@@ -340,7 +340,8 @@ public class MessagingManagerApply {
 	ArrayList<String> getResourceUsers(Connection sd, String fileName, int oId) throws SQLException {
 		
 		ArrayList<String> users = new ArrayList<String> ();
-		String sql = "select distinct u.ident "
+		
+		String sqlQuestionResources = "select distinct u.ident "
 				+ "from users u, user_project up, user_group ug, project p, translation t, survey s "
 				+ "where u.id = up.u_id "
 				+ "and u.id = ug.u_id "
@@ -352,22 +353,44 @@ public class MessagingManagerApply {
 				+ "and ug.g_id = 3 "		// enum
 				+ "and (t.type = 'image' or t.type = 'video' or t.type = 'audio')"			
 				+ "and t.value = ?";
-
-		PreparedStatement pstmt = null;
+		PreparedStatement pstmtQuestionResouces = null;
+		
+		String sqlSurveyResources = "select distinct u.ident "
+				+ "from users u, user_project up, user_group ug, project p, survey s "
+				+ "where u.id = up.u_id "
+				+ "and u.id = ug.u_id "
+				+ "and up.p_id = p.id "
+				+ "and s.p_id = p.id "
+				+ "and p.o_id = ? "
+				+ "and not u.temporary "
+				+ "and ug.g_id = 3 "		// enum
+				+ "and s.manifest like ?";			
+		PreparedStatement pstmtSurveyResouces = null;
 		
 		try {
-			pstmt = sd.prepareStatement(sql);
-			pstmt.setInt(1, oId);
-			pstmt.setString(2, fileName);
-			log.info("Get resource users: " + pstmt.toString());
-			ResultSet rs = pstmt.executeQuery();
+			// Question level
+			pstmtQuestionResouces = sd.prepareStatement(sqlQuestionResources);
+			pstmtQuestionResouces.setInt(1, oId);
+			pstmtQuestionResouces.setString(2, fileName);
+			log.info("Get question level resource users: " + pstmtQuestionResouces.toString());
+			ResultSet rs = pstmtQuestionResouces.executeQuery();
+			while(rs.next()) {
+				users.add(rs.getString(1));
+			}
+			
+			// Survey level
+			pstmtSurveyResouces = sd.prepareStatement(sqlSurveyResources);
+			pstmtSurveyResouces.setInt(1, oId);
+			String modName = fileName.replace("_", "!_").replace("%", "!%").replace("!", "!!");
+			pstmtSurveyResouces.setString(2, "%" + modName + "%");
+			log.info("Get survey level resource users: " + pstmtSurveyResouces.toString());
+			rs = pstmtSurveyResouces.executeQuery();
 			while(rs.next()) {
 				users.add(rs.getString(1));
 			}
 		} finally {
-
-			try {if (pstmt != null) {	pstmt.close();}} catch (SQLException e) {}
-
+			try {if (pstmtQuestionResouces != null) {	pstmtQuestionResouces.close();}} catch (SQLException e) {}
+			try {if (pstmtSurveyResouces != null) {	pstmtSurveyResouces.close();}} catch (SQLException e) {}
 		}
 		
 		return users;
