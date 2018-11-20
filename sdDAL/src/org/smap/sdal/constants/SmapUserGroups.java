4b8c3f886c3174bb9c8f692ac4a18932511c8094
Neil Penman <neilpenman@gmail.com>
2018-11-20 13:40:45 +1000

    Auto refresh the user management page if a user updates their own details.  Hopefully fix issue with security groups being lost while moving between organisations

---
+++ sdDAL/src/org/smap/sdal/constants/SmapUserGroups.java
@@ -0,0 +1,14 @@
+package org.smap.sdal.constants;
+
+public class SmapUserGroups {
+
+	public static final int ADMIN = 1;
+	public static final int ANALYST = 2;
+	public static final int ENUM = 3;
+	public static final int ORG_ADMIN = 4;
+	public static final int MANAGE = 5;
+	public static final int SECURITY = 6;
+	public static final int VIEW_DATA = 7;
+	public static final int ENTERPRISE = 8;
+	public static final int OWNER = 8;
+}
