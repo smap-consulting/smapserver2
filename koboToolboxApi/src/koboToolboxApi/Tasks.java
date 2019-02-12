c23c22c4b8e91ca70b2ae3820747dc82e6d292cf
Neil Penman <neilpenman@gmail.com>
2019-02-12 13:16:30 +1000

    Add 401 error page. Does not work and page is just a copy of the home page

--- koboToolboxApi/src/koboToolboxApi/Tasks.java
+++ koboToolboxApi/src/koboToolboxApi/Tasks.java
@@ -25,7 +25,6 @@ import com.google.gson.GsonBuilder;
 
 import java.sql.Connection;
 import java.util.ArrayList;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
