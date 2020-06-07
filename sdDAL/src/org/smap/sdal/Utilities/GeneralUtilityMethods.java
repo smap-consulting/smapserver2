39455f0f3a41c5ef5f9255e666fbcaf0aaa15d0d
Neil Penman <neilpenman@gmail.com>
2020-06-07 15:55:51 +1000

    Do not change name of preexisting media files

--- sdDAL/src/org/smap/sdal/Utilities/GeneralUtilityMethods.java
+++ sdDAL/src/org/smap/sdal/Utilities/GeneralUtilityMethods.java
@@ -469,6 +469,10 @@ public class GeneralUtilityMethods {
 
 		log.info("Create attachments");
 
+		if(srcName.startsWith("attachments/")) {
+			// An existing image in an updated record that was not changed
+			srcName = srcName.substring(srcName.lastIndexOf('/'));
+		}
 		String value = null;
 		String srcExt = "";
 		String srcBase = srcName;
