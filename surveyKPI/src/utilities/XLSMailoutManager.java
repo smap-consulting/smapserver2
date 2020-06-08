c6c71481058f396fa2b14312c0889b902761a881
Neil Penman <neilpenman@gmail.com>
2020-06-08 10:06:28 +1000

    Allow question names with capitals in mailout initial data

--- surveyKPI/src/utilities/XLSMailoutManager.java
+++ surveyKPI/src/utilities/XLSMailoutManager.java
@@ -21,7 +21,6 @@ along with SMAP.  If not, see <http://www.gnu.org/licenses/>.
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
