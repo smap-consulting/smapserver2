810f51d878722cacce53c3d509d0f4b2a02e652a
Neil Penman <neilpenman@gmail.com>
2020-08-27 21:35:27 +1000

    Add indexeddb to service worker

--- myWork/WebContent/myWorkServiceWorker.js
+++ myWork/WebContent/myWorkServiceWorker.js
@@ -1,9 +1,14 @@
 
+var cachename = 'v3';
+var databaseName = 'mywork';
+var dbversion = 1;
+var idbSupported = typeof indexedDB !== 'undefined';
+
 // During the installation phase, you'll usually want to cache static assets.
 self.addEventListener('install', function(e) {
 	// Once the service worker is installed, go ahead and fetch the resources to make this work offline.
 	e.waitUntil(
-		caches.open('v3').then(function(cache) {
+		caches.open(cachename).then(function(cache) {
 			return cache.addAll([
 				'./index.html',
 				'./css/bootstrap.v4.5.min.css',
@@ -20,16 +25,23 @@ self.addEventListener('install', function(e) {
 
 // when the browser fetches a URL…
 self.addEventListener('fetch', function(event) {
-	// … either respond with the cached object or go ahead and fetch the actual URL
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
+
+	if (event.request.url.includes("/surveyKPI/")) {
+		// response to API requests, Cache Update Refresh strategy
+	} else {
+		// response to static files requests, Cache-First strategy
+		event.respondWith(
+			caches.match(event.request).then(function(response) {
+				return response || fetch(event.request);
+			})
+		);
+	}
+
+
 });
 
 self.addEventListener('activate', function (event) {
-	var cacheKeeplist = ['v3'];
+	var cacheKeeplist = [cachename];
 
 	event.waitUntil(
 		caches.keys().then(function (keyList) {
@@ -40,4 +52,34 @@ self.addEventListener('activate', function (event) {
 			}));
 		})
 	);
+
+	event.waitUntil(
+		createDB()
+	);
 });
+
+function createDB() {
+	return new Promise((resolve, reject) => {
+
+		if(idbSupported) {
+			var request = indexedDB.open(databaseName, dbversion);
+
+			request.onerror = function (event) {
+				reject();
+			};
+
+			request.onsuccess = function (event) {
+				resolve();
+			};
+
+			request.onupgradeneeded = function(event) {
+				var db = event.target.result;
+				db.createObjectStore("forms");
+			};
+
+		} else {
+			resolve();
+		}
+
+	});
+}
