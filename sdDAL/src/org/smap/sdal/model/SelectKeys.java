1112ed79bdc0bcbf05e71683d7021f4a09f66e60
Neil Penman <neilpenman@gmail.com>
2020-09-17 17:51:19 +1000

    Get keys for multi languages in lookup choices

---
+++ sdDAL/src/org/smap/sdal/model/SelectKeys.java
@@ -0,0 +1,8 @@
+package org.smap.sdal.model;
+
+import java.util.HashMap;
+
+public class SelectKeys {
+	public String valueColumn;
+	public HashMap<String, String> labelColumns = new HashMap<> ();	// Label columns keyed by language
+}
