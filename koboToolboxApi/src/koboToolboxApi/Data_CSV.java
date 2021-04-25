2613a0d23be3122c8b0836c23ba6a31e95146341
Neil Penman <neilpenman@gmail.com>
2021-04-25 10:13:46 +1000

    filter added to data.csv api requests

--- koboToolboxApi/src/koboToolboxApi/Data_CSV.java
+++ koboToolboxApi/src/koboToolboxApi/Data_CSV.java
@@ -182,6 +182,7 @@ public class Data_CSV extends Application {
 			@QueryParam("filename") String filename, 
 			@QueryParam("audit") String audit_set,
 			@QueryParam("tz") String tz,			// Time Zone
+			@QueryParam("filter") String filter,
 			@QueryParam("merge_select_multiple") String merge 	// If set to yes then do not put choices from select multiple questions in separate columns
 			) throws ApplicationException, Exception {
 
@@ -437,7 +438,7 @@ public class Data_CSV extends Application {
 						null	,	// key filter
 						tz,
 						null	,	// instance id
-						null,	// advanced filter
+						filter,	// advanced filter
 						null,	// Date filter name
 						null,	// Start date
 						null		// End date
