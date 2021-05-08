714d6512afae6211056a856c1ae6560b08f972b8
Neil Penman <neilpenman@gmail.com>
2021-05-08 11:06:52 +1000

    Add editing of email settings to settings page

---
+++ sdDAL/src/org/smap/sdal/model/EmailSettings.java
@@ -0,0 +1,30 @@
+package org.smap.sdal.model;
+
+/*
+This file is part of SMAP.
+
+SMAP is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SMAP is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SMAP.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+public class EmailSettings {
+	public String admin_email;
+	public String smtp_host;
+	public String email_domain;
+	public String email_user;
+	public String email_password;
+	public int email_port;
+	public String default_email_content;
+	public String server_description;
+}
