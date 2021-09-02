22e789b2a448a40582b00eef58259fc3354e0742
Neil Penman <neilpenman@gmail.com>
2021-09-02 10:21:35 +1000

    Initial updates to support java 11 in development environment

--- sdDAL/src/org/smap/sdal/model/XformsJavaRosa.java
+++ sdDAL/src/org/smap/sdal/model/XformsJavaRosa.java
@@ -1,13 +1,8 @@
 package org.smap.sdal.model;
 
 import java.util.ArrayList;
-import java.util.Date;
-
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSeeAlso;
-import javax.xml.bind.annotation.XmlType;
 
 @XmlRootElement(name="xforms")
 @XmlSeeAlso({ODKForm.class})
