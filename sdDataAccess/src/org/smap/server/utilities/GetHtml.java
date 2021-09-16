c72e2b79ed0fcdfb548323fa45e74b39d5717cac
Neil Penman <neilpenman@gmail.com>
2021-09-16 11:15:55 +1000

    Add authenticate to http config.  Add order by to csv extract to match use in fieldTask

--- sdDataAccess/src/org/smap/server/utilities/GetHtml.java
+++ sdDataAccess/src/org/smap/server/utilities/GetHtml.java
@@ -1942,4 +1942,32 @@ public class GetHtml {
 		return count;
 	}
 	
+	/*
+	 * Remove data casts these are required for database expressions but not for xpath
+	 */
+	private String removeCasts(String in) {
+		StringBuilder sb = new StringBuilder("");
+		
+		if(in == null) {
+			return null;
+		} else {
+			int lastIdx = 0;
+			int idx = in.indexOf("cast(", 0);
+			while(idx >= 0 && idx < in.length()) {
+				sb.append(in.substring(lastIdx, idx));
+				lastIdx = in.indexOf("#{", idx);
+				if(lastIdx >= 0) {
+					idx = in.indexOf('}', lastIdx);
+					if(idx >= 0) {
+						sb.append(in.substring(lastIdx, idx));
+						idx = in.indexOf(')', idx);
+						if(idx >= 0) {
+							idx++;
+						}
+					}
+				}
+			}
+		}
+		return sb.toString();
+	}
 }
