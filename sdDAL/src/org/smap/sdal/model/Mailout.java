692857a661a3cd664d9ea775cff6328b91cd8033
Neil Penman <neilpenman@gmail.com>
2021-09-24 09:52:28 +1000

    Update database with multiple select value

--- sdDAL/src/org/smap/sdal/model/Mailout.java
+++ sdDAL/src/org/smap/sdal/model/Mailout.java
@@ -7,14 +7,16 @@ public class Mailout {
 	public String name;
 	public String subject;
 	public String content;
+	public boolean ms;
 	
 	public MailoutLinks links;
 	
-	public Mailout(int id, String surveyIdent, String name, String subject, String content) {
+	public Mailout(int id, String surveyIdent, String name, String subject, String content, boolean ms) {
 		this.id = id;
 		this.survey_ident = surveyIdent;
 		this.name = name;
 		this.subject = subject;
 		this.content = content;
+		this.ms = ms;
 	}
 }
