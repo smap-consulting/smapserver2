fa1baf79fc8bb7a0bedb5efa85af1c2fc44f4c26
Neil Penman <neilpenman@gmail.com>
2021-10-01 14:05:00 +1000

    Aligned classes used to communicate with fieldTask to those used on fieldTask

--- surveyKPI/src/taskModel/TrAssignment.java
+++ surveyKPI/src/taskModel/TrAssignment.java
@@ -1,11 +1,10 @@
-package org.smap.sdal.model;
+package taskModel;
 
-public class Assignment {
+public class TrAssignment {
 	public int assignment_id;
 	public String assignment_status;
 	public String task_comment;
 	public int task_id;
 	public long dbId;		// included to match fieldTask definition
 	public String uuid;		// The identifier of the data created by this task
-	public User user;
 }
---
+++ surveyKPI/src/taskModel/TrTask.java
@@ -0,0 +1,31 @@
+package taskModel;
+
+import java.util.Date;
+
+public class TrTask {
+	public int id;
+	public String title;
+    public String name;
+	public String url;
+	public String form_id;
+	public int form_version;
+    public String update_id;
+    public String initial_data_source;   // task || survey || none
+	public Date scheduled_at;
+    public Date scheduled_finish;
+	public String location_trigger;
+	public boolean repeat;			// Task can be completed multiple times
+	public String address;			// Key value pairs representing an unstructured address
+	public String status;
+	public int show_dist;
+	
+	
+	//public String pid;				// Project id
+	
+	// Backward compatability
+	public String type;
+	
+	// Local attributes - used only on device
+    public String initial_data;
+
+}
