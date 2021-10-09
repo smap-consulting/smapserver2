53fda7e38b92f013f876cf3dfc3e206518b7b453
Neil Penman <neilpenman@gmail.com>
2021-10-09 10:14:41 +1000

    Add background reports table

---
+++ sdDAL/src/org/smap/sdal/model/BackgroundReport.java
@@ -0,0 +1,15 @@
+package org.smap.sdal.model;
+
+import java.sql.Timestamp;
+
+public class BackgroundReport {
+	public int id;
+	public int oId;
+	public int uId;
+	public String status;
+	public String status_msg;
+	public String report_type;
+	public String report_name;
+	public String details;	
+	public Timestamp start_time;
+}
