d88c3ad6fee9cfa5fe52097b25fd61b955a9956c
Neil Penman <neilpenman@gmail.com>
2021-11-07 08:35:10 +1000

    Fix issues with monthly reporting, report name and remove old report service

--- sdDAL/src/org/smap/sdal/managers/BackgroundReportsManager.java
+++ sdDAL/src/org/smap/sdal/managers/BackgroundReportsManager.java
@@ -59,7 +59,7 @@ public class BackgroundReportsManager {
 	
 	public static String PARAM_O_ID = "oId";
 	public static String PARAM_MONTH = "month";
-	public static String PARAM_YEAR = "month";
+	public static String PARAM_YEAR = "year";
 	public static String PARAM_BY_SURVEY = "bySurvey";
 	public static String PARAM_BY_PROJECT = "byProject";
 	public static String PARAM_BY_DEVICE = "byDevice";
