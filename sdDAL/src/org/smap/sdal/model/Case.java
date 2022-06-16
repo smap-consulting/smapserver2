baebcdeedde47d766f4945faba23ec43a269e4bb
Neil Penman <neilpenman@gmail.com>
2022-06-17 08:38:19 +1000

    Create _thread, _thread_created, _assigned, _alert when tables are created

--- sdDAL/src/org/smap/sdal/model/Case.java
+++ sdDAL/src/org/smap/sdal/model/Case.java
@@ -1,10 +1,12 @@
 package org.smap.sdal.model;
 
 public class Case {
+	public int id;	// primary key of the record
 	public String title;
 	public String instanceid;
 	
-	public Case(String title, String instanceid) {
+	public Case(int id, String title, String instanceid) {
+		this.id = id;
 		this .title = title;
 		this.instanceid = instanceid;
 	}
