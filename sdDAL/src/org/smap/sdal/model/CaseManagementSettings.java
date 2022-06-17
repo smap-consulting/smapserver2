746fdd9a5fb84c2f9eb13d2213dffd50c4686a2f
Neil Penman <neilpenman@gmail.com>
2022-06-17 14:14:47 +1000

    Add criticality question to case model

--- sdDAL/src/org/smap/sdal/model/CaseManagementSettings.java
+++ sdDAL/src/org/smap/sdal/model/CaseManagementSettings.java
@@ -3,4 +3,5 @@ package org.smap.sdal.model;
 public class CaseManagementSettings {
 	public String finalStatus;
 	public String statusQuestion;	// Column name of the status question
+	public String criticalityQuestion;	// Column name of the status question
 }
