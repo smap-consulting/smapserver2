a790ada3e275713e75e7d12703cef85ad63e69ce
Neil Penman <neilpenman@gmail.com>
2022-06-18 14:19:26 +1000

    Update version to 22.06

--- setup/deploy/patchdb.sh
+++ setup/deploy/patchdb.sh
@@ -318,5 +318,5 @@ echo "\COPY language_codes (code, aws_translate, aws_transcribe, transcribe_defa
 
 
 # update version reference
-new_version="2111"
+new_version="2206"
 echo "$new_version" > ~/smap_version
