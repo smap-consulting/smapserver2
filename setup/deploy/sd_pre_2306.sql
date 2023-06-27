9e1f9814b926bab3c307689575b4278370443a68
Neil Penman <neilpenman@gmail.com>
2023-06-28 08:27:33 +1000

    Remove delete of csvtable entries on update

--- setup/deploy/sd_pre_2306.sql
+++ setup/deploy/sd_pre_2306.sql
@@ -357,8 +357,6 @@ alter table csvtable add column linked_sident text;
 alter table csvtable add column chart_key text;
 alter table csvtable add column linked_s_pd boolean;
 
-delete from csvtable where survey;
-
 alter table organisation add column ft_bg_stop_menu boolean default false;
 update organisation set ft_exit_track_menu = false where ft_bg_stop_menu is null;
 
