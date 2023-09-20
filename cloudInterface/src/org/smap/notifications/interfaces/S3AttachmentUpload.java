02a2b428e49dd2cbca9f49a527062a766b7ea914
Neil Penman <neilpenman@gmail.com>
2023-09-20 20:15:20 +1000

    Add S3Attachmentupload

---
+++ cloudInterface/src/org/smap/notifications/interfaces/S3AttachmentUpload.java
@@ -0,0 +1,28 @@
+package org.smap.notifications.interfaces;
+
+import java.io.IOException;
+
+/*
+ * Static class to upload attachments to S3 
+ * Fast (hopefully)
+ */
+public class S3AttachmentUpload {
+
+	private S3AttachmentUpload() {
+		
+	}
+	
+	
+	public static void put(String basePath, String filePath) throws Exception {
+		
+	}
+	
+	public static void get(String basePath, String filePath) throws IOException {
+		
+	}
+	
+	private static void initialise(String basePath) {
+
+	}
+	
+}
