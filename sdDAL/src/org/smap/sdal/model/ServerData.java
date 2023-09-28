8a42126246ab0961b8900b6b49d1a6dd1cbda7e4
Neil Penman <neilpenman@gmail.com>
2023-09-28 21:57:31 +1000

    Add support for managing of ratelimit from the client

--- sdDAL/src/org/smap/sdal/model/ServerData.java
+++ sdDAL/src/org/smap/sdal/model/ServerData.java
@@ -11,6 +11,7 @@ public class ServerData {
 	public String google_key;
 	public String maptiler_key;
 	public String sms_url;
+	public int ratelimit;
 	public double password_strength;
 	public int keep_erased_days;
 	public String css;
