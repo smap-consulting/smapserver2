d2645496163f2ae96101e69d8e26f0e8c171fc8b
Neil Penman <neilpenman@gmail.com>
2023-11-24 09:47:27 +1000

    Update to version 23.11

--- setup/deploy/patchdb.sh
+++ setup/deploy/patchdb.sh
@@ -333,5 +333,5 @@ echo "\COPY language_codes (code, aws_translate, aws_transcribe, transcribe_defa
 
 
 # update version reference
-new_version="2310"
+new_version="2311"
 echo "$new_version" > ~/smap_version
--- setup/install/install.sh
+++ setup/install/install.sh
@@ -402,7 +402,7 @@ sudo apt-get install gdal-bin -y
 sudo apt-get install ttf-dejavu -y
 
 # Add a file containing the version number
-echo "2310" > ~/smap_version
+echo "2311" > ~/smap_version
 
 echo '##### 21. Add postgres and apache to tomcat group'
 if [ "$DBHOST" = "127.0.0.1" ]; then
