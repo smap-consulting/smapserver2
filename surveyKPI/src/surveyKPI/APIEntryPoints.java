e1091bfb6d7e5ed1e5fb2e3070485eb075d5f496
Neil Penman <neilpenman@gmail.com>
2025-03-03 14:38:54 +1000

    remove unused import

--- surveyKPI/src/surveyKPI/APIEntryPoints.java
+++ surveyKPI/src/surveyKPI/APIEntryPoints.java
@@ -67,7 +67,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
