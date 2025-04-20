d27244ffbf02dae8d5d7d65f736057c92343242d
Neil Penman <neilpenman@gmail.com>
2025-04-20 13:26:27 +1000

    Update version number

--- setup/deploy/patchdb.sh
+++ setup/deploy/patchdb.sh
@@ -49,7 +49,7 @@ if [ $a24 -eq 0 ]; then
 	exit 1;
 fi
 
-version=2501
+version=2504
 if [ -e /smap_bin/smap_version ]; then
         version=`sudo cat /smap_bin/smap_version`
 elif [ -e ~/smap_version ]; then
@@ -358,4 +358,4 @@ full_version=`cat ./full_version`
 echo "update server set version = '$full_version'" | sudo -i -u postgres $PSQL -q -d survey_definitions 2>&1
 
 # update version reference
-echo "2501" > /smap_bin/smap_version
+echo "2504" > /smap_bin/smap_version
