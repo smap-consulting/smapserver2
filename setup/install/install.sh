0ec8198bb3a5f7019632030542ea8006e0f775a5
Neil Penman <neilpenman@gmail.com>
2025-06-27 09:23:38 +0800

    fix tomcat version issue

--- setup/install/install.sh
+++ setup/install/install.sh
@@ -116,7 +116,7 @@ if [ $u2404 -eq 1 ]; then
     sudo groupadd tomcat
     sudo useradd -s /bin/false -g tomcat -d /usr/share/tomcat9 tomcat
     echo 'get tomcat'
-    wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.106/bin/apache-tomcat-9.0.93.tar.gz
+    wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.106/bin/apache-tomcat-9.0.106.tar.gz
     sudo mkdir /usr/share/tomcat9
     sudo tar xzf apache-tomcat-9*tar.gz -C /usr/share/tomcat9 --strip-components=1
     rm apache-tomcat-9*tar.gz
