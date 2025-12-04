0194ab91961be3f28e4b704d4f5bb696feeb85ae
Neil Penman <neilpenman@gmail.com>
2025-12-04 13:27:01 +1000

    Use bundle name specified in bundle settings in survey list

--- sdDAL/src/org/smap/sdal/model/CaseManagementSettings.java
+++ sdDAL/src/org/smap/sdal/model/CaseManagementSettings.java
@@ -1,7 +1,9 @@
 package org.smap.sdal.model;
 
 public class CaseManagementSettings {
+	public String name;					// Name of the Bundle / Topic that is hosting the case
+	public String description;			// Description of the Bundle / Topic
 	public String finalStatus;
-	public String statusQuestion;	// Column name of the status question
+	public String statusQuestion;		// Column name of the status question
 	public String criticalityQuestion;	// Column name of the status question
 }
