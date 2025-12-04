b8a8b3271832beff855b22d8b693a1425669c476
Neil Penman <neilpenman@gmail.com>
2025-12-04 19:55:27 +1000

    Removed unused extension of ArrayList

--- sdDAL/src/org/smap/sdal/model/XformsJavaRosa.java
+++ sdDAL/src/org/smap/sdal/model/XformsJavaRosa.java
@@ -6,14 +6,11 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 
 @XmlRootElement(name="xforms")
 @XmlSeeAlso({ODKForm.class})
-public class XformsJavaRosa extends ArrayList<ODKForm> {
+public class XformsJavaRosa {
 	
 	//@XmlAttribute
 	//public String xmlns = "http://openrosa.org/xforms/xformsList";
 	
 	public ArrayList<ODKForm> xform;
 	
-	public XformsJavaRosa() {
-		super();		
-	}
 }
