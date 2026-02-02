0d722f29b011bc6d10381e5d5f96300b48961738
Neil Penman <neilpenman@gmail.com>
2026-02-03 08:54:12 +1000

    check for existence of setenv.sh before updating it

--- setup/deploy/patchdb.sh
+++ setup/deploy/patchdb.sh
@@ -324,9 +324,11 @@ then
     done
 fi
 sudo cp  $deploy_from/resources/properties/setcredentials.sh /smap_bin
-envset=`cat /var/lib/$TOMCAT_VERSION/bin/setenv.sh | grep -c "setcredentials"`
-if [ $envset -eq 0 ]; then
-	echo ". /smap_bin/setcredentials.sh" | sudo tee -a /var/lib/$TOMCAT_VERSION/bin/setenv.sh 
+if [ ! -f /var/lib/$TOMCAT_VERSION/bin/setenv.sh ]; then
+	envset=`cat /var/lib/$TOMCAT_VERSION/bin/setenv.sh | grep -c "setcredentials"`
+	if [ $envset -eq 0 ]; then
+		echo ". /smap_bin/setcredentials.sh" | sudo tee -a /var/lib/$TOMCAT_VERSION/bin/setenv.sh 
+	fi
 fi
 
 
