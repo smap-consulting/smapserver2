d9ebd64a7381ad8d466f768ba4752b547451b1a2
Neil Penman <neilpenman@gmail.com>
2026-07-21 10:48:48 +1000

    Fix typo in a comment

--- sdDAL/src/org/smap/sdal/model/Template.java
+++ sdDAL/src/org/smap/sdal/model/Template.java
@@ -5,7 +5,7 @@ public class Template {
 	public boolean fromSettings;		// legacy, due to storage of single template in settings
 	public String name;					// Template name
 	public String templateType;			// pdf || word
-	public boolean not_available;		// Set true of the pdf template is not available for use
+	public boolean not_available;		// Set true if the pdf template is not available for use
 	public boolean default_template;
 	public String filepath;
 	public String rule;
